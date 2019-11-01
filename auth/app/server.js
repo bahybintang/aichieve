8c698590b81bac5878a215ad622fab3955d7cc64
bahybintang@gmail.com <bahybintang@gmail.com>
2019-11-01 18:25:41 +0700

    mas angger coba fix docker :((

--- auth/app/server.js
+++ auth/app/server.js
@@ -7,9 +7,27 @@ const bcrypt = require('bcrypt')
 const User = require('./model/user')
 const jwt = require('jsonwebtoken')
 dotenv.config()
-mongoose.connect(`mongodb://aichieve-mongodb:27017/aichieve`, { useNewUrlParser: true }, (err) => {
-    if (err) throw(err)
-})
+
+const options = {
+    autoIndex: false,
+    reconnectTries: 100,
+    reconnectInterval: 500,
+    poolSize: 10,
+    bufferMaxEntries: 0
+}
+
+const connectWithRetry = () => {
+    console.log('MongoDB connection with retry');
+    mongoose.connect(`mongodb://aichieve-mongodb:27017/aichieve`, options)
+    .then(() => {
+        console.log('MongoDB is connected')
+    })
+    .catch(err => {
+        console.log('MongoDB connection unsuccessful');
+        setTimeout(connectWithRetry, 1000)
+    });
+}
+connectWithRetry()
 
 var db = mongoose.connection
 
@@ -19,25 +37,25 @@ app.use(bodyParser.json());
 app.post('/auth/register', (req, res) => {
     if (req.body.name != undefined && req.body.username != undefined && req.body.password != undefined) {
         User.findOne({ username: req.body.username })
-        .then(data => {
-            if (data == undefined) {
-                var user = new User({ name: req.body.name, username: req.body.username, password: bcrypt.hashSync(req.body.password, 10) })
-                user.save(err => {
-                    if (err) {
-                        res.send(JSON.stringify({ status: "failed", message: "failed save to db" }))
-                    }
-                    else {
-                        res.send(JSON.stringify({ status: "success", token: jwt.sign({ username: req.body.username }, process.env.KEY) }))
-                    }
-                })
-            }
-            else {
-                res.send(JSON.stringify({ status: "failed", message: "username already taken" }))
-            }
-        })
-        .catch(err => {
-            res.send(JSON.stringify({ status: "failed", message: ".something error on our side" }))
-        })
+            .then(data => {
+                if (data == undefined) {
+                    var user = new User({ name: req.body.name, username: req.body.username, password: bcrypt.hashSync(req.body.password, 10) })
+                    user.save(err => {
+                        if (err) {
+                            res.send(JSON.stringify({ status: "failed", message: "failed save to db" }))
+                        }
+                        else {
+                            res.send(JSON.stringify({ status: "success", token: jwt.sign({ username: req.body.username }, process.env.KEY) }))
+                        }
+                    })
+                }
+                else {
+                    res.send(JSON.stringify({ status: "failed", message: "username already taken" }))
+                }
+            })
+            .catch(err => {
+                res.send(JSON.stringify({ status: "failed", message: ".something error on our side" }))
+            })
     }
     else {
         res.send(JSON.stringify({ status: "failed", message: "pliss fill all slurr :((((" }))
@@ -47,22 +65,24 @@ app.post('/auth/register', (req, res) => {
 app.post('/auth/login', (req, res) => {
     if (req.body.username != undefined && req.body.password != undefined) {
         User.findOne({ username: req.body.username })
-        .then(data => {{
-            if (data == undefined) {
-                res.send(JSON.stringify({ status: "failed", message: "username or password salah :((" }))
-            }
-            else {
-                if (bcrypt.compareSync(req.body.password, data.password)) {
-                    res.send(JSON.stringify({ status: "success", token: jwt.sign({ username: req.body.username }, process.env.KEY) }))
+            .then(data => {
+                {
+                    if (data == undefined) {
+                        res.send(JSON.stringify({ status: "failed", message: "username or password salah :((" }))
+                    }
+                    else {
+                        if (bcrypt.compareSync(req.body.password, data.password)) {
+                            res.send(JSON.stringify({ status: "success", token: jwt.sign({ username: req.body.username }, process.env.KEY) }))
+                        }
+                        else {
+                            res.send(JSON.stringify({ status: "failed", message: "username or password salah :((" }))
+                        }
+                    }
                 }
-                else {
-                    res.send(JSON.stringify({ status: "failed", message: "username or password salah :((" }))
-                }
-            }
-        }})
-        .catch(err => {
-            res.send(JSON.stringify({ status: "failed", message: ".something error on our side" }))
-        })
+            })
+            .catch(err => {
+                res.send(JSON.stringify({ status: "failed", message: ".something error on our side" }))
+            })
     }
     else {
         res.send(JSON.stringify({ status: "failed", message: "pliss fill all slurr :((((" }))
--- idea/app/server.js
+++ idea/app/server.js
@@ -6,9 +6,27 @@ const Ideas = require('./model/idea')
 const User = require('./model/user')
 const bodyParser = require('body-parser')
 dotenv.config()
-mongoose.connect(`mongodb://aichieve-mongodb:27017/aichieve`, { useNewUrlParser: true }, (err) => {
-    if (err) throw (err)
-})
+
+const options = {
+    autoIndex: false,
+    reconnectTries: 100,
+    reconnectInterval: 500,
+    poolSize: 10,
+    bufferMaxEntries: 0
+}
+
+const connectWithRetry = () => {
+    console.log('MongoDB connection with retry')
+    mongoose.connect(`mongodb://aichieve-mongodb:27017/aichieve`, options)
+    .then(() => {
+        console.log('MongoDB is connected')
+    })
+    .catch(err => {
+        console.log('MongoDB connection unsuccessful')
+        setTimeout(connectWithRetry, 1000)
+    });
+}
+connectWithRetry()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,29 +34,29 @@ app.use(bodyParser.json());
 app.post('/idea/:userID/add', (req, res) => {
     if (req.body.title != undefined && req.body.description != undefined) {
         User.findOne({ username: req.params.userID })
-        .then(data => {
-            if (data == undefined) {
-                res.send(JSON.stringify({ status: "failed", message: "user not found" }))
-            }
-            else {
-                var idea = new Ideas({
-                    userID: req.params.userID,
-                    title: req.body.title,
-                    description: req.body.description
-                })
-                idea.save(err => {
-                    if (err) {
-                        res.send(JSON.stringify({ status: "failed", message: "fail add to db" }))
-                    }
-                    else {
-                        res.send(JSON.stringify({ status: "success" }))
-                    }
-                })
-            }
-        })
-        .catch(err => {
-            res.send(JSON.stringify({ status: "failed", message: ".something error on our side" }))
-        })
+            .then(data => {
+                if (data == undefined) {
+                    res.send(JSON.stringify({ status: "failed", message: "user not found" }))
+                }
+                else {
+                    var idea = new Ideas({
+                        userID: req.params.userID,
+                        title: req.body.title,
+                        description: req.body.description
+                    })
+                    idea.save(err => {
+                        if (err) {
+                            res.send(JSON.stringify({ status: "failed", message: "fail add to db" }))
+                        }
+                        else {
+                            res.send(JSON.stringify({ status: "success" }))
+                        }
+                    })
+                }
+            })
+            .catch(err => {
+                res.send(JSON.stringify({ status: "failed", message: ".something error on our side" }))
+            })
     }
     else {
         res.send(JSON.stringify({ status: "failed", message: "Pliss fill all slurr :((" }))
@@ -56,12 +74,12 @@ app.get('/idea/get', (req, res) => {
     }
 
     Ideas.find(query)
-    .then(result => {
-        res.send({ status: "success", data: result })
-    })
-    .catch(err => {
-        res.send(JSON.stringify({ status: "failed", message: ".something error on our side" }))
-    })
+        .then(result => {
+            res.send({ status: "success", data: result })
+        })
+        .catch(err => {
+            res.send(JSON.stringify({ status: "failed", message: ".something error on our side" }))
+        })
 })
 
 app.get('/idea/')
