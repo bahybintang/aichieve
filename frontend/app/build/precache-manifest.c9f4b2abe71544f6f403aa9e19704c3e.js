3712f69c679799bdd91da0a6aede915eb552401a
bahybintang@gmail.com <bahybintang@gmail.com>
2019-11-01 14:57:12 +0700

    benerin static

---
+++ frontend/app/build/precache-manifest.c9f4b2abe71544f6f403aa9e19704c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42aa65fdd53f83db459062bd2447efc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af3aedb5f40720fdff7a",
+    "url": "/static/css/main.327d3c0d.chunk.css"
+  },
+  {
+    "revision": "95a311f2ff6e94058e4d",
+    "url": "/static/js/2.ef4c7ec1.chunk.js"
+  },
+  {
+    "revision": "af3aedb5f40720fdff7a",
+    "url": "/static/js/main.ee1fc0f7.chunk.js"
+  },
+  {
+    "revision": "846b3d6a0bd73f513000",
+    "url": "/static/js/runtime-main.03038b7f.js"
+  }
+]);
--- frontend/app/server.js
+++ frontend/app/server.js
@@ -3,10 +3,10 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 
-app.use(express.static(__dirname + "./public"))
+app.use(express.static(__dirname + "./build"))
 
 app.get(['/', '/login'], (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
+    res.sendFile(__dirname + '/build/index.html')
 })
 
 app.listen(process.env.PORT, () => console.log(`Idea app listening on port ${process.env.PORT}!`))
