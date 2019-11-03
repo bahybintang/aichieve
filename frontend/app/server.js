3303be94d0e87ea7b755f773ed56ac72d808fade
bahybintang@gmail.com <bahybintang@gmail.com>
2019-11-03 11:14:56 +0700

    s

--- frontend/app/server.js
+++ frontend/app/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 app.use(express.static(__dirname + "./build"))
+app.use(express.static(__dirname + "./public"))
 
 app.get(['/', '/login'], (req, res) => {
     res.sendFile(__dirname + '/build/index.html')
